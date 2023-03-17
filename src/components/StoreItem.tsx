2fbd62fef18e89b9fd10c316e374673d3f9f8ba8
Karthik Ramesh <karthikramesh909545@gmail.com>
2023-03-17 14:06:22 +0530

    second commit - fixed a few bugs

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -26,12 +26,12 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         src={imgUrl}
         height="200px"
         style={{
-          objectFit: "cover",
+          objectFit: "contain",
         }}
       />
       <Card.Body className="d-flex flex-column">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-          <span className="fs-2">{name}</span>
+          <span className="fs-5">{name}</span>
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
         <div className="mt-auto">
