d4b2204e51446330c44c836e84585d1d71ad28d3
Karthik Ramesh <karthikramesh909545@gmail.com>
2023-03-17 11:18:32 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Services from "./pages/Services";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+
+function App() {
+  return (
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Store />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  );
+}
+
+export default App;
